ba7c2e33a82fc40400db7129f61686e9f17a793f
Alessandro Trenta <alessandrotrenta.30@gmail.com>
2022-10-13 18:47:47 +0200

    Analyze results now is ok for non-terminated runs

--- analyze_results.py
+++ analyze_results.py
@@ -11,16 +11,18 @@ for f in onlyfiles:
     currf = open(join(path, f), "r")
     csv_raw = csv.reader(currf)
     data = list(csv_raw)[-1]
-    curr_rew = float(data[1])
-    curr_delta = [float(data[i]) for i in range(2, 2+delta_len)]
-    curr_lambda = [float(data[i]) for i in range(2+delta_len, 2+delta_len+lambda_len)]
-    if curr_rew > best_reward:
-        best_run = int(data[0])
-        best_reward = curr_rew
-        deltas = curr_delta
-        lambdas = curr_lambda
+    if len(data)>10:
+        curr_rew = float(data[1])
+        curr_delta = [float(data[i]) for i in range(2, 2+delta_len)]
+        curr_lambda = [float(data[i]) for i in range(2+delta_len, 2+delta_len+lambda_len)]
+        if curr_rew > best_reward:
+            best_run = float(data[0])
+            best_reward = curr_rew
+            deltas = curr_delta
+            lambdas = curr_lambda
     currf.close()
     
+print(f'Best run: {best_run}')
 print(f'Best reward: {best_reward}')
 print(f'Deltas: {deltas}')
 print(f'Lambdas: {lambdas}')
