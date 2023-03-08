744fc312a647772d4600d168e4de78bf63c0b597
Alessandro Trenta <alessandrotrenta.30@gmail.com>
2023-03-08 01:33:49 +0100

    Plotting scripts finished, last step is to clean

--- analyze_BPS_hybrid.py
+++ analyze_BPS_hybrid.py
@@ -8,32 +8,35 @@ from matplotlib import pyplot as plt
 import itertools
 import os
 import re
+import seaborn as sns
 
+rew_to_take = 10
 gs = np.concatenate((np.arange(start=0.01, stop=0.25, step=0.01),
                      np.arange(start=0.25, stop=4.05, step=0.05),
                      #np.arange(start=4.25, stop=5.25, step=0.25)
                      ))
 gs = np.around(gs, decimals=2)
-g = 1.
+g = 1
 g_index = np.argwhere(gs==g)[0]
-
-path = join('/data/trenta', 'results_BPS_deltaunk_3fix')
-save_path = join('.', 'BPS_hybrid_3fix_analyzed')
+OPE_fix=3
+path = join('.', 'results_BPS', f'results_BPS_deltaunk_3fix')
+prefix = 'g1'
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 r = re.compile('sac[0-9]+.csv')
 onlyfiles = list(filter(r.match, onlyfiles))
+
+#onlyfiles = onlyfiles[:400]
+
 n_files = len(onlyfiles)
-
+#print(onlyfiles)
 best_reward = 0.
-
 delta_len = 15
 lambda_len = 15
 
-lambda_fix = 3
 
 rewards = np.zeros(n_files)
-OPEs = np.zeros((n_files, lambda_len-lambda_fix))
-deltas = np.zeros((n_files, 5))
+OPEs = np.zeros((n_files, lambda_len-OPE_fix))
+deltas = np.zeros((n_files, delta_len - 10))
 
 for i, f in enumerate(onlyfiles):
     currf = open(join(path, f))
@@ -52,48 +55,121 @@ for i, f in enumerate(onlyfiles):
     curr_OPE = [float(data[i]) for i in range(5+delta_len, 5+delta_len+lambda_len)]
 
     rewards[i] = curr_rew
-    OPEs[i] = curr_OPE[lambda_fix:]
+    OPEs[i] = curr_OPE[OPE_fix:]
     deltas[i] = curr_delta[10:]
         
     currf.close()
     
 orderer = np.argsort(rewards)
-
+orderer = np.flip(orderer)
 OPEs_ordered = OPEs[orderer]
 deltas_ordered = deltas[orderer]
 
-best_rew_to_take = 25
+analysis_path = f'./BPS_analyzed_hybrid/{prefix}'
+if not os.path.exists(analysis_path):
+        os.makedirs(analysis_path)
+OPE_vals = OPEs_ordered[:rew_to_take]
+delta_vals = deltas_ordered[:rew_to_take]
+OPE_means = np.mean(OPE_vals, axis=0)
+OPE_stds = np.std(OPE_vals, axis=0)
+delta_means = np.mean(delta_vals, axis=0)
+delta_stds = np.std(delta_vals, axis=0)
 
-vals = OPEs_ordered[-best_rew_to_take:]
-OPE_means = np.mean(vals, axis=0)
-OPE_stds = np.std(vals, axis=0)
-print(OPE_means)
-print(OPE_stds)
+with open(join(analysis_path, f'{prefix}_OPE{OPE_fix+1}_10_{rew_to_take}.txt'), 'w') as f:
+    print(f'Best {rew_to_take} rewards', file=f)
+    print('OPE means:', file=f)
+    print(OPE_means, file=f)
+    print('OPE stds:', file=f)
+    print(OPE_stds, file=f)
+    print('std relative to mean', file=f)
+    print(100*OPE_stds/OPE_means, file=f)
+    print('delta means:', file=f)
+    print(delta_means, file=f)
+    print('OPE stds:', file=f)
+    print(delta_stds, file=f)
+    print('std relative to mean', file=f)
+    print(100*delta_stds/delta_means, file=f)
 
-plt.figure()
-for i in range(lambda_fix+1, lambda_len+1):
-    plt.scatter(np.ones(best_rew_to_take)*i, vals[:, i-lambda_fix-1], color='blue')
-plt.plot(range(lambda_fix+1, lambda_len+1), OPE_means, color='orange')
-plt.errorbar(range(lambda_fix+1, lambda_len+1), OPE_means, yerr=OPE_stds, color='orange')
-plt.xlabel('index')
-plt.ylabel('OPE[index]')
-plt.title(f'OPE coefficients for best {best_rew_to_take} tries')
-plt.savefig(f'hybrid_OPE4_15_{best_rew_to_take}.jpg')
+
+operators = np.arange(start=OPE_fix+1, stop=lambda_len+1)
+fig, ax = plt.subplots()
+### Average plot
+# Initialize the figure
+ax = sns.pointplot(
+    x=operators, y=OPE_means, color='orange',
+    join=False, dodge=.8 - .8 / 3,
+    markers="d", scale=.75, errorbar=None, label='Experimental mean', ax=ax
+)
+x_coords = []
+y_coords = []
+for point_pair in ax.collections:
+    for x, y in point_pair.get_offsets():
+        x_coords.append(x)
+        y_coords.append(y)
+ax.errorbar(x_coords, y_coords, yerr=OPE_stds,
+    color='orange', fmt=' ', zorder=1)
+# Show each observation with a scatterplot
+for j in range(rew_to_take):
+    sns.stripplot(
+        x=operators, y=OPEs_ordered[j], color='blue',
+        dodge=True, alpha=.25, zorder=-1, legend=False
+    )
+# Show the conditional means, aligning each pointplot in the
+# center of the strips by adjusting the width allotted to each
+# category (.8 by default) by the number of hue levels
+plt.legend()
+# Improve the legend
+sns.move_legend(
+    ax, loc="upper right", ncol=1, frameon=True, columnspacing=1, handletextpad=0
+)
+plt.xlabel('Operator number')
+plt.ylabel('OPE coefficient')
+#plt.yscale('log')
+plt.title(f'OPE coefficients on best {rew_to_take} runs, {OPE_fix} coefficient fixed, g={g}')
+plt.savefig(join(analysis_path, f'{prefix}_OPE{OPE_fix+1}_10_{rew_to_take}.jpg'), dpi=300)
+
+#plt.show()
+plt.close()
 
 
-vals = deltas_ordered[-best_rew_to_take:]
-delta_means = np.mean(vals, axis=0)
-delta_stds = np.std(vals, axis=0)
-print(delta_means)
-print(delta_stds)
+operators = np.arange(start=11, stop=delta_len+1)
+fig, ax = plt.subplots()
+### Average plot
+# Initialize the figure
+ax = sns.pointplot(
+    x=operators, y=delta_means, color='orange',
+    join=False, dodge=.8 - .8 / 3,
+    markers="d", scale=.75, errorbar=None, label='Experimental mean', ax=ax
+)
+x_coords = []
+y_coords = []
+for point_pair in ax.collections:
+    for x, y in point_pair.get_offsets():
+        x_coords.append(x)
+        y_coords.append(y)
+ax.errorbar(x_coords, y_coords, yerr=delta_stds,
+    color='orange', fmt=' ', zorder=1)
+# Show each observation with a scatterplot
+for j in range(rew_to_take):
+    sns.stripplot(
+        x=operators, y=deltas_ordered[j], color='blue',
+        dodge=True, alpha=.25, zorder=-1, legend=False
+    )
+# Show the conditional means, aligning each pointplot in the
+# center of the strips by adjusting the width allotted to each
+# category (.8 by default) by the number of hue levels
+plt.legend()
+# Improve the legend
+sns.move_legend(
+    ax, loc="upper right", ncol=1, frameon=True, columnspacing=1, handletextpad=0
+)
+plt.xlabel('Operator number')
+plt.ylabel('Scaling dimension')
+#plt.yscale('log')
+plt.title(f'Scaling dimensions on best {rew_to_take} runs, {OPE_fix} coefficient fixed, g={g}')
+plt.savefig(join(analysis_path, f'{prefix}delta11_{delta_len}_{rew_to_take}.jpg'), dpi=300)
 
-plt.figure()
-for i in range(10, 15):
-    plt.scatter(np.ones(best_rew_to_take)*i, vals[:, i-10], color='blue')
-plt.plot(range(10, 15), delta_means, color='orange')
-plt.errorbar(range(10, 15), delta_means, yerr=delta_stds, color='orange')
-plt.xlabel('index')
-plt.ylabel('delta[index]')
-plt.title(f'delta for best {best_rew_to_take} tries')
-plt.savefig(f'hybrid_delta10_15_{best_rew_to_take}.jpg')
+#plt.show()
+plt.close()
+
 
