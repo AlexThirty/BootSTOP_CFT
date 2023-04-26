b61054f576cde8e4241f6dd6ae59b2fe604b9d33
Alessandro Trenta <alessandrotrenta.30@gmail.com>
2023-04-26 15:24:42 +0200

    Updated some graphs

--- analyze_BPS_results.py
+++ analyze_BPS_results.py
@@ -9,39 +9,39 @@ import os
 import re
 
 
-OPE_first = 9
-OPE_second = 10
+OPE_first = 5
+OPE_second = 6
 best_rew_to_take = 25
 best_reward = 0.
 delta_len = 10
 lambda_len = 10
 lambda_fix = 1
-analysis_path = 'BPS_analyzed_results_weak'
-g_list = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
-#g_list = [1., 1.5, 2., 2.5, 3., 3.5, 4.]
+analysis_path = 'BPS_analyzed_results'
+#g_list = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
+g_list = [1., 1.5, 2., 2.5, 3., 3.5, 4.]
 #g_list = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.5, 2., 2.5, 3., 3.5, 4.]
 path_list = [
-    join('.', 'results_BPS', 'results_BPS_1fix_g005'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g010'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g015'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g020'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g025'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g030'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g035'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g040'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g045'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g05'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g06'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g07'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g08'),
-    join('.', 'results_BPS', 'results_BPS_1fix_g09'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g005'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g010'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g015'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g020'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g025'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g030'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g035'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g040'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g045'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g05'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g06'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g07'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g08'),
+    #join('.', 'results_BPS', 'results_BPS_1fix_g09'),
     join('.', 'results_BPS', 'results_BPS_3fix_g1'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g15'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g2'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g25'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g3'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g35'),
-    #join('.', 'results_BPS', 'results_BPS_3fix_g4')
+    join('.', 'results_BPS', 'results_BPS_3fix_g15'),
+    join('.', 'results_BPS', 'results_BPS_3fix_g2'),
+    join('.', 'results_BPS', 'results_BPS_3fix_g25'),
+    join('.', 'results_BPS', 'results_BPS_3fix_g3'),
+    join('.', 'results_BPS', 'results_BPS_3fix_g35'),
+    join('.', 'results_BPS', 'results_BPS_3fix_g4')
 ]
 experiments = len(path_list)
 
